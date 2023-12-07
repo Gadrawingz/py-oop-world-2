3d86b8d358b5fcf47750ba687080449c1e2232ec
Gadrawingz <legmeekmc4@gmail.com>
2023-12-08 00:01:50 +0200

    Second commit of the night

--- inheritance/inherit_basic1.py
+++ inheritance/inherit_basic1.py
@@ -1,11 +1,22 @@
 # My Base class
 class Animal:
-    task = ["One", "Two"]
-    def eat(self):
-        global task        
-        print(f'Task {task[0]}')
-        
-    def walk(self):
-        global task        
-        print(f'Task {task[0]}')
     
+    def eat(self):      
+        print(f'Animal: Eating')
+        
+    def walk(self):     
+        print(f'Animal: Walking')
+    
+# Creation of derived class
+class Dog(Animal):
+    def bark(self):
+        print("Dog Act: Barking!")
+
+# Create object of the Dog class
+dog1 = Dog()
+# Calling members of the base class
+dog1.eat()
+dog1.walk()
+
+# Calling member of the derived class
+dog1.bark()
