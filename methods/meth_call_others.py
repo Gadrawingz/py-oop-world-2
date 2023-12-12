81bd324f942b2e6778c66571b87f6fc0711729be
Gadrawingz <legmeekmc4@gmail.com>
2023-12-12 23:53:45 +0200

    First commit on method that calls another methods

---
+++ methods/meth_call_others.py
@@ -0,0 +1,10 @@
+# Here I am creating a method that call other methods
+# Here I have a class called a Bag
+class Bag:
+    def __init__(self):
+        # This will contain data
+        self.data = []
+        
+    def add_data(self, stuff):
+        self.data = stuff
+    
